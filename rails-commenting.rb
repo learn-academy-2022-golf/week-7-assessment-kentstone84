cda4efc2ce4de519858e2ac40e8cb636a4902eeb
Kent Stone <stone@Kents-MacBook-Air.local>
2022-12-12 11:32:07 -0500

    Completed week 7 assessment.

--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,26 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1) The BlogPostsController inherits from the ApplicationController
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+
+  # ---2) The index method is used to display all the blog posts
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3) The show method is used to display a single blog post
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) The new method is used to create a new blog post
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5) The create method is used to save a new blog post
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -32,13 +33,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    # ---6)
+    # ---6) The edit method is used to edit a blog post
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7) The update method is used to save the changes made to a blog post
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -48,15 +49,15 @@ class BlogPostsController < ApplicationController
   def destroy
     @post = BlogPost.find(params[:id])
     if @post.destroy
-      # ---8)
+      # ---8) The destroy method is used to delete a blog post
       redirect_to blog_posts_path
     end
   end
 
-  # ---9)
+  # ---9) The private method is used to restrict access to the methods below it
   private
   def blog_post_params
-    # ---10)
+    # ---10) The blog_post_params method is used to require and permit the title and content of a blog post
     params.require(:blog_post).permit(:title, :content)
   end
 end
